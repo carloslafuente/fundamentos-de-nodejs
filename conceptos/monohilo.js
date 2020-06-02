1f150a529ec22560bcccd9bdbf2db22137ddc900
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-02 12:21:50 -0400

    Installed PM2

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -6,9 +6,12 @@ setInterval(() => {
   console.log(i);
   i++;
 
-  if (i === 5) {
-    var a = 3 + z;
-  }
+  // console.log('Crasheando el sistema');
+  // if (i === 5) {
+  //   var a = 3 + z;
+  // }
 
   // console.log('Sigo activo');
 }, 1000);
+
+console.log('Ultima instruccion');
